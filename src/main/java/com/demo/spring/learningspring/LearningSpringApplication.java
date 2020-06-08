84d94c95ca063ad78f00844545ef87b93fbe338f
Priya Raut <rautpriya215@gmail.com>
2020-06-08 13:58:03 +0530

    Optimize imports.

--- src/main/java/com/demo/spring/learningspring/LearningSpringApplication.java
+++ src/main/java/com/demo/spring/learningspring/LearningSpringApplication.java
@@ -1,18 +1,7 @@
 package com.demo.spring.learningspring;
 
-import com.demo.spring.learningspring.data.entity.Guest;
-import com.demo.spring.learningspring.data.entity.Reservation;
-import com.demo.spring.learningspring.data.entity.Room;
-import com.demo.spring.learningspring.data.repository.GuestRepository;
-import com.demo.spring.learningspring.data.repository.ReservationRepository;
-import com.demo.spring.learningspring.data.repository.RoomRepository;
-import org.hibernate.annotations.GeneratorType;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringApplication {
@@ -20,5 +9,5 @@ public class LearningSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringApplication.class, args);
 	}
-	
+
 }
